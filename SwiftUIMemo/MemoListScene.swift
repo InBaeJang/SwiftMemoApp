b452dd95d8d5d768481e86488bb38d90901f4dc6
InBaeJang <inbaejj117@gmail.com>
2020-11-07 16:05:09 +0900

    List Design

--- SwiftUIMemo/MemoListScene.swift
+++ SwiftUIMemo/MemoListScene.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
+    @EnvironmentObject var formatter: DateFormatter
     
     var body: some View {
         NavigationView {
             List(store.list){ memo in
-                Text(memo.content)
+                VStack(alignment:.leading) { // 아래에 추가되는 내용들이 vertical하게 추가됨
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    
+                    Text("\(memo.insertDate, formatter: self.formatter)")
+                        .font(.caption)
+                        //이렇게 뒤에 붙은 걸 identifier라 한다.
+                        .foregroundColor(Color(UIColor.secondaryLabel))
+                }
             }
             .navigationBarTitle("내 메모")
         }//List에 키를 두고 cmd+shift+a 키
@@ -22,6 +32,9 @@ struct MemoListScene: View {
 
 struct MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
-        MemoListScene().environmentObject(MemoStore())
+        MemoListScene()
+            .environmentObject(MemoStore())
+            .environmentObject(DateFormatter.memoDateFormatter)
+            
     }
 }
